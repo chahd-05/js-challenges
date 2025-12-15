4abd7a467e6fc0868800fe9669ec8a120c4e0140
shahd <mazouzchahd12@gmail.com>
2025-12-15 09:04:10 +0100

    feat : JavaScript object-1

--- chall-object.js
+++ chall-object.js
@@ -55,3 +55,125 @@ for (let i = 0; i < users.length; i++){
     }
 }
 console.log(user);
+
+// 4. Trouver l’utilisateur le plus jeune :
+
+//  A: reduce
+const youngest = users.reduce((min, old) => old.age < min.age ? old : min);
+console.log(youngest);
+
+
+//  B: boucle + comparaison
+let youngeste = users[0];
+
+for (let i = 0; i < users.length; i++){
+    if (users[i].age < youngeste.age){
+        youngeste = users[i];
+    }
+}
+console.log(youngeste);
+
+// 5. Trouver l’utilisateur le plus âgé :
+
+//  A: reduce
+const oldest = users.reduce((max, old) => old.age > max.age ? old : max);
+console.log(oldest);
+
+
+//  B: boucle
+let oldeste = users[0];
+
+for (let i = 0; i < users.length; i++){
+    if (users[i].age > oldeste.age){
+        oldeste = users[i];
+    }
+}
+console.log(oldeste);
+
+
+// 6. Calculer la somme des scores :
+
+//  A: reduce
+const somme = users.reduce((sum, e) => sum + e.score, 0);
+console.log(somme);
+
+
+//  B: boucle
+let total = 0;
+
+for (let i = 0; i < users.length; i++){
+    total = total + users[i].score
+}
+console.log(total);
+
+
+// 7. Calculer la moyenne d’âge :
+
+//  A: reduce
+const moyenne = users.reduce((moy, e) => moy + e.age, 0) / users.length;
+console.log(moyenne);
+
+//  B: boucle
+let average = 0;
+
+for (let i = 0; i < users.length; i++){
+    average = average + users[i].age;
+}
+const avg = average / users.length;
+console.log(avg);
+
+// 8. Vérifier si tous les utilisateurs ont un score > 30 :
+
+//  A: every
+const Vérifier = users.every(e => e.score > 30);
+console.log(Vérifier);
+
+//  B: boucle
+let controle = false;
+
+for (let i = 0; i < users.length; i++){
+    if (users[i].score > 30);
+    controle = true;
+    break;
+}
+console.log(controle);
+
+// 9. Vérifier si au moins un utilisateur est de Agadir :
+
+//  A: some
+const Agadir = users.some(e => e.city === 'Agadir');
+console.log(Agadir);
+
+//  B: boucle
+let agcity = false;
+
+for (let i = 0; i < users.length; i++){
+    if (users[i].city === 'Agadir'){
+        agcity = true;
+        break;
+    }
+}
+console.log(agcity);
+
+
+// 10. Obtenir la liste des id :
+
+//  A: map
+const liste = users.map(e => e.id);
+console.log(liste);
+
+//  B: boucle
+const list = [];
+
+for (let i = 0; i < users.length; i++){
+    list.push(users[i].id);
+}
+console.log(list)
+
+
+// 11. Filtrer les utilisateurs ayant un score supérieur à 50
+//  A: filter
+//  B: boucle + push
+// 12. Filtrer les utilisateurs vivant à Marrakech
+//  A: filter
+//  B: boucle
